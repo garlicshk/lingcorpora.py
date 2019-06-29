a08e1802917fdc7dc2b14af381e108b1ff253ab5
OneAdder <mikivo@list.ru>
2019-06-29 09:16:55 +0300

    authors list fixed in setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,18 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/lingcorpora/lingcorpora.py',
-    author='Ekaterina Gerasimenko, Artem Kopetsky, Alexey Koshevoy,' \
-        'Mark Sobolev, Anna Zueva, Diana Malyshok, Maria Terekhina,' \
-        'Ustinya Kosheleva and George Moroz',
+    author=\
+        'Alexey Koshevoy, Anna Klezovich, ' \
+        'Anna Zueva, Artem Kopetsky, ' \
+        'Diana Malyshok, Ekaterina Gerasimenko, ' \
+        'George Moroz, Maria Terekhina, ' \
+        'Mark Sobolev, Michael Voronov, ' \
+        'Ustinya Kosheleva',
     author_email='katgerasimenko@gmail.com',
     license='MIT',
     packages=['lingcorpora', 'lingcorpora.corpora'],
     python_requires='>=3.5',
     zip_safe=False,
-    keywords = ['corpora', 'api', 'language'],
+    keywords=['corpora', 'api', 'language'],
     install_requires=['bs4', 'requests', 'lxml', 'tqdm']
 )
