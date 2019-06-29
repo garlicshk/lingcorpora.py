2d4486a2ef187265532b870471833c223bbc7839
OneAdder <mikivo@list.ru>
2019-06-29 09:07:40 +0300

    long_description for PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 from lingcorpora import __version__
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
     name='lingcorpora',
     version=__version__,
     description='API for text corpora',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/lingcorpora/lingcorpora.py',
     author='Ekaterina Gerasimenko, Artem Kopetsky, Alexey Koshevoy,' \
         'Mark Sobolev, Anna Zueva, Diana Malyshok, Maria Terekhina,' \
